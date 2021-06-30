1b522c2a2d11c5315c1b415177346d9b3d327206
Benjamin Janssens <benji.janssens@gmail.com>
2021-06-30 13:14:44 +0200

    Added ESLint and Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+/* eslint-disable no-console */
+
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 const port = 5000;
 
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: "Hello World" });
+app.use('/', (req: Request, res: Response) => {
+  res.status(200).send({ data: 'Hello World' });
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
