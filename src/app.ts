e0296d97f643af7560714c395df481e234bc2a8d
Benjamin Janssens <benji.janssens@gmail.com>
2021-07-08 11:19:16 +0200

    Added Swagger

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import errorMiddleware from 'middleware/error.middleware';
 import morgan from 'morgan';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 import router from './routes';
 import env from './utils/env';
 
@@ -19,5 +21,23 @@ app.use(morgan(env.MORGAN_FORMAT));
 app.use(router);
 
 app.use(errorMiddleware);
+
+if (env.isDev)
+  app.use(
+    '/swagger',
+    swaggerUI.serve,
+    swaggerUI.setup(
+      swaggerJSDoc({
+        swaggerDefinition: {
+          openapi: '3.0.0',
+          info: {
+            title: 'nodejs-express-typescript',
+            version: '1.0.0',
+          },
+        },
+        apis: ['./src/swagger/**/*', './src/routes/**/*.router.ts'],
+      })
+    )
+  );
 
 export default app;
--- src/routes/ping.router.ts
+++ src/routes/ping.router.ts
@@ -3,6 +3,24 @@ import { Router } from 'express';
 
 const router = Router();
 
+/**
+ * @swagger
+ *
+ * /ping:
+ *   get:
+ *     tags:
+ *       - Examples
+ *     responses:
+ *       200:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Pong!
+ */
 router.get('/', PingController.ping);
 
 export default router;
